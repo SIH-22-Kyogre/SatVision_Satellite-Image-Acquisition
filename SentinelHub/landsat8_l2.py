106c09e115e223a60692b351c1885427b132bc39
karthik-d <karthikdesingu2000@gmail.com>
2022-08-23 06:59:23 +0530

    process api queries templated

--- SentinelHub/landsat8_l2.py
+++ SentinelHub/landsat8_l2.py
@@ -1,5 +1,59 @@
+import json
+
 from token_manager import *
 
 session, token_info = get_oauth_session(gen_token=False)
-print(session)
-print(token_info)
+# print(session)
+# print(token_info)
+
+
+response = session.post(
+    'https://services.sentinel-hub.com/api/v1/process',
+    data = json.dumps({
+        "input": {
+            "bounds": {
+                "bbox": [
+                    13.822174072265625,
+                    45.85080395917834,
+                    14.55963134765625,
+                    46.29191774991382
+                ]
+            },
+            "data": [{
+                "type": "sentinel-2-l2a"
+            }]
+        },
+        "output": {
+            "responses": [{
+                "identifier": "default",
+                "format": {
+                    "type": "image/png"
+                }
+            }]
+        },
+        "evalscript": """
+
+        function setup() {
+            return {
+            input: ["B02", "B03", "B04"],
+            output: {
+                bands: 3
+            }
+            };
+        }
+
+        function evaluatePixel(
+            sample,
+            scenes,
+            inputMetadata,
+            customData,
+            outputMetadata
+        ) {
+            return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+        }
+        """
+    })
+)
+
+print(response)
+print(response.content)
